f709a0a664236c9d6e7c8faf4f0281f255125d00
Cameron Gallarno <cgallarno@gmail.com>
2013-04-06 16:47:02 -0400

    wahtever

---
+++ App/Plugins/script/module.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('.menu').click(function(){
+		console.log('sdfsdfsdfsdfsdf');
+	});
+	// $('.menu').toggle(
+	// 		    function() {
+	// 		        console.log('toggle');
+
+	// 		        $('#wrapper, #header').animate({ left: 160 }, 150);
+	// 		        $('.menu').addClass('active');
+	// 		    }, 
+	// 		    function() {
+	// 		        $('#wrapper, #header').animate({ left: 0 }, 150);
+	// 		        $('.menu').removeClass('active');
+	// 		    }
+	// 	    );
+});
+
+
+		
